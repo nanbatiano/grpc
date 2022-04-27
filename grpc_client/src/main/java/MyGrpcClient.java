a847c578ff749576b3a285bf9680bfdad1db99b1
shenpp <shenpengpeng@hualala.com>
2022-04-27 14:02:33 +0800

    master commit 10

--- grpc_client/src/main/java/MyGrpcClient.java
+++ grpc_client/src/main/java/MyGrpcClient.java
@@ -14,7 +14,7 @@ public class MyGrpcClient {
                 HelloRequest.newBuilder()
                         .setName("shenpengpeng")
                         .setAge(28155)
-                        .addHobbies("football master dev")
+                        .addHobbies("football master dev121")
                         .putTags( "how?","wonderful" )
                         .build());
         System.out.println(helloResponse);
